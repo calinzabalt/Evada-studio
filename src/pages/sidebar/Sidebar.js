62df1fdd8f9d13344d5904454f33fdc56ff320f3
calinzabalt <calinzabalt@gmail.com>
2020-07-09 17:46:46 +0300

    add tools on sidebar

--- src/pages/sidebar/Sidebar.js
+++ src/pages/sidebar/Sidebar.js
@@ -43,6 +43,43 @@ class Sidebar extends React.Component{
 								</Link>
 							</li>
 						</ul>
+						<div className="dropdown">
+						<ul>
+							<button className="dropbtn">Tools</button>
+							<div className="dropdown_content">
+							<li>
+								<Link className={"nav-link"} to={"/projects"}>
+									Projects
+								</Link>
+							</li>
+							<li>
+								<Link className={"nav-link"} to={"/projects"}>
+									Link One
+								</Link>
+							</li>
+							<li>
+								<Link className={"nav-link"} to={"/projects"}>
+									Link Two
+								</Link>
+							</li>
+							<li>
+								<Link className={"nav-link"} to={"/projects"}>
+									Link Three
+								</Link>
+							</li>
+							<li>
+								<Link className={"nav-link"} to={"/projects"}>
+									Link Four
+								</Link>
+							</li>
+							</div>
+						</ul>
+						<div className="buttons">
+							<button>EXAMPLE 1</button>
+							<button>EXAMPLE 2</button>
+							<button>EXAMPLE 3</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		)
